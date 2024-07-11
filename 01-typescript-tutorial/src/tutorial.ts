8ad63e111c1272e55ed69caacf1471cf041a69af
robertmeredith <sushiaudio@hotmail.com>
2024-07-11 10:47:23 +1000

    Initial commit

---
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -0,0 +1,15 @@
+console.log('Typescript Tutorial')
+
+// interface someValue {
+//   name: string
+//   id: number
+// }
+
+// let someObj: someValue = {
+//   name: 'random',
+//   id: 123,
+// }
+
+// console.log(someObj)
+
+
